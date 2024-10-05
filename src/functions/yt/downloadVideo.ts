40f18405c2f3a6d4301ebc21a849728d51bc6819
S B Rakesh Rath <work.sbrakeshrath@gmail.com>
2024-10-06 01:56:34 +0530

    testing volumes

--- src/functions/yt/downloadVideo.ts
+++ src/functions/yt/downloadVideo.ts
@@ -7,7 +7,8 @@ import uploadMergedVideos from "../../storage/uploadMergedVideo.js";
 
 //create a unique file name for the video which is not present in the temp folder
 function createUniqueFileName(extension: string) {
-  const tempDir = os.tmpdir() + "/";
+  // const tempDir = os.tmpdir() + "/";
+  const tempDir = "/tmp2"
   const fileName =
     Math.random().toString(36).substring(2, 15) +
     Math.random().toString(36).substring(2, 15);
